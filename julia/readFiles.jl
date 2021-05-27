4c5c5b3e887ee833c943f4055f3665f6b2022fde
drew <andresalazar138@gmail.com>
2021-05-27 00:01:17 -0500

    added graphing functions to the analysis section in the jupyter notebook

--- julia/readFiles.jl
+++ julia/readFiles.jl
@@ -24,23 +24,23 @@ end
 
 function runBenchmarks()
     # naive
-    # writeTimes(5, "Hamrle1.mtx", naiveMultiply, "naive")
-    # writeTimes(5, "GD99_b.mtx", naiveMultiply, "naive")
-    # writeTimes(5, "can_256.mtx", naiveMultiply, "naive")
-    # writeTimes(5, "dwa512.mtx", naiveMultiply, "naive")
-    # writeTimes(5, "delaunay_n10.mtx", naiveMultiply, "naive")
+    writeTimes(5, "Hamrle1.mtx", naiveMultiply, "naive")
+    writeTimes(5, "GD99_b.mtx", naiveMultiply, "naive")
+    writeTimes(5, "can_256.mtx", naiveMultiply, "naive")
+    writeTimes(5, "dwa512.mtx", naiveMultiply, "naive")
+    writeTimes(5, "delaunay_n10.mtx", naiveMultiply, "naive")
     # # strassens
-    writeTimes(5, "Hamrle1.mtx", strassen, "strassens")
-    writeTimes(5, "GD99_b.mtx", strassen, "strassens")
-    writeTimes(5, "can_256.mtx", strassen, "strassens")
-    writeTimes(5, "dwa512.mtx", strassen, "strassens")
-    writeTimes(5, "delaunay_n10.mtx", strassen, "strassens")
+    writeTimes(5, "Hamrle1.mtx", strassen, "strassen")
+    writeTimes(5, "GD99_b.mtx", strassen, "strassen")
+    writeTimes(5, "can_256.mtx", strassen, "strassen")
+    writeTimes(5, "dwa512.mtx", strassen, "strassen")
+    writeTimes(5, "delaunay_n10.mtx", strassen, "strassen")
     # dictionary of keys
-    # writeTimes(5, "Hamrle1.mtx", sparseMultiplication, "sparse")
-    # writeTimes(5, "GD99_b.mtx", sparseMultiplication, "sparse")
-    # writeTimes(5, "can_256.mtx", sparseMultiplication, "sparse")
-    # writeTimes(5, "dwa512.mtx", sparseMultiplication, "sparse")
-    # writeTimes(5, "delaunay_n10.mtx", sparseMultiplication, "sparse")
+    writeTimes(5, "Hamrle1.mtx", sparseMultiplication, "sparse")
+    writeTimes(5, "GD99_b.mtx", sparseMultiplication, "sparse")
+    writeTimes(5, "can_256.mtx", sparseMultiplication, "sparse")
+    writeTimes(5, "dwa512.mtx", sparseMultiplication, "sparse")
+    writeTimes(5, "delaunay_n10.mtx", sparseMultiplication, "sparse")
 end
 
 runBenchmarks()
